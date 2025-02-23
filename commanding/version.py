c5f91a120895f69de96dd1596d699c67bcf083c9
Mohammed Al-Baqer <mohmmadkasper@gmail.com>
2025-02-23 22:11:14 +0300

    Update version.py

--- commanding/version.py
+++ commanding/version.py
@@ -1,16 +1,14 @@
 def version_App():
-    __version__ = "3.1.0"
-    __release_date__ = "2025-01-09"
+    __version__ = "3.1.1"
+    __release_date__ = "Copyright (c) 2024-2025"
     __description__ = "This is the second release of the application with new features and improvements."
     __author__ = "Mohammed Al-Baqer"
-    __email__ = "example@example.com"
     __status__ = "Stable"
     print("\033[97;1m")
-    print(__version__)
-    print(__release_date__)
-    print(__description__)
-    print(__author__)
-    print(__email__)
-    print(__status__)
+    print('Version : ' + __version__)
+    print('Release Date : ' + __release_date__)
+    print('Description : ' + __description__)
+    print('Author : ' + __author__)
+    print('Status : ' + __status__)
 if __name__ == "__main__":
     version_App()
