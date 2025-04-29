f6f9aac4175837a73b9f4a78382962402f1113d7
Mohammed Al-Baqer <mohmmadkasper@gmail.com>
2025-04-29 13:28:50 +0300

    Update python-3.11.0rc2-amd64.bat

--- commanding/python-3.11.0rc2-amd64.bat
+++ commanding/python-3.11.0rc2-amd64.bat
@@ -1,5 +1,5 @@
 :: Custom License Based on MIT License
-:: Update     : Update Programming Manager Pc
+:: Update     : chacking setup python Programming Manager Pc
 :: Version    : 3.2.1
 :: Github     : https://github.com/wsl-iq/Manager-Pc
 :: Developer  : Mohammed Al-Baqer
@@ -126,10 +126,6 @@
 
 :: THE SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND EXPRESS OR IMPLIED INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
 :: IN NO EVENT SHALL THE AUTHOR OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM DAMAGES OR OTHER LIABILITY WHETHER IN AN ACTION OF CONTRACT TORT OR OTHERWISE ARISING FROM OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-:: ================================================================
-::                  END OF PRIVACY POLICY
-:: ================================================================
 
 
 @echo off
